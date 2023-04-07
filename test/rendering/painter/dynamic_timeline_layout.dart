5b9d19121d3c9703c6c09cc6dc0628e8b169399c
Markus Herrmann <mherrmann@solid-3d.de>
2023-04-07 22:51:55 +0200

    - added layout test for axis direction

--- test/rendering/painter/dynamic_timeline_layout.dart
+++ test/rendering/painter/dynamic_timeline_layout.dart
@@ -24,7 +24,7 @@ class DynamicTimelineLayoutTests {
       test(
         'Computer size with fixed cross axis item extent '
         '--> maxCrossAxisIndicatorExtent+ '
-            '(crossAxisSpacing+maxCrossAxisItemExtent)*2 = 144',
+        '(crossAxisSpacing+maxCrossAxisItemExtent)*2 = 144',
         () {
           final layoutEngine = _produceLayoutEngine(maxCrossAxisItemExtent: 40);
           final newSize = layoutEngine.computeSize(
@@ -42,8 +42,8 @@ class DynamicTimelineLayoutTests {
 
       test(
         'Computer size with fixed main axis item extent smaller than constraint'
-            '--> chooses the calculated value (smaller than the constraint)',
-            () {
+        '--> chooses the calculated value (smaller than the constraint)',
+        () {
           final layoutEngine = _produceLayoutEngine();
           final newSize = layoutEngine.computeSize(
             constraints: const BoxConstraints(
@@ -54,13 +54,13 @@ class DynamicTimelineLayoutTests {
             ),
           );
           //
-          expect(newSize.height<500,true );
+          expect(newSize.height < 500, true);
         },
       );
       test(
         'Computer size with fixed main axis item extent bigger than constraint'
-            '--> chooses the constraint value',
-            () {
+        '--> chooses the constraint value',
+        () {
           final layoutEngine = _produceLayoutEngine();
           final newSize = layoutEngine.computeSize(
             constraints: const BoxConstraints(
@@ -70,23 +70,48 @@ class DynamicTimelineLayoutTests {
               minWidth: 300,
             ),
           );
-          //
-          expect(newSize.height,100 );
+          expect(newSize.height, 100);
         },
       );
 
+      test(
+        'Switching the axis from horizontal to vertical '
+        '--> size width and height are getting also switched',
+        () {
+          const sizeConstraint = BoxConstraints(
+            maxHeight: 500,
+            minHeight: 500,
+            maxWidth: 300,
+            minWidth: 300,
+          );
+          final layoutEngineHorizontal =
+              _produceLayoutEngine(axis: Axis.horizontal, maxCrossAxisItemExtent: 40);
+          final layoutEngineVertical =
+              _produceLayoutEngine(axis: Axis.vertical, maxCrossAxisItemExtent: 40);
+
+          final newSizeHorizontal = layoutEngineHorizontal.computeSize(
+            constraints: sizeConstraint,
+          );
+          final newSizeVertical = layoutEngineVertical.computeSize(
+            constraints: sizeConstraint,
+          );
+
+          expect(newSizeHorizontal.height, newSizeVertical.width);
+          expect(newSizeHorizontal.width, newSizeVertical.height);
+        },
+      );
     });
   }
 
   static DynamicTimelineLayout _produceLayoutEngine(
-      {double maxCrossAxisItemExtent = double.infinity}) {
+      {double maxCrossAxisItemExtent = double.infinity, Axis axis = Axis.vertical}) {
     return DynamicTimelineLayout(
       intervalExtent: 50,
       crossAxisCount: 2,
       crossAxisSpacing: 2,
       maxCrossAxisIndicatorExtent: 60,
       maxCrossAxisItemExtent: maxCrossAxisItemExtent,
-      axis: Axis.vertical,
+      axis: axis,
       firstDateTime: DateTime(2023, 1, 1, 7),
       lastDateTime: DateTime(2023, 1, 1, 7 + 3),
       intervalDuration: const Duration(hours: 1),
